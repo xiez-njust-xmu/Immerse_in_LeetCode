15dfb4b1222ba0b6ddd101107edda090dc2ed9ba
xiez-njust-xmu <xiez2000@163.com>
2024-11-17 16:58:04 +0800

    添加项目文件。

---
+++ Immerse_in_LeetCode.cpp
@@ -0,0 +1,64 @@
+﻿// Immerse_in_LeetCode.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+/*
+825. 适龄的朋友
+在社交媒体网站上有 n 个用户。给你一个整数数组 ages ，其中 ages[i] 是第 i 个用户的年龄。
+
+如果下述任意一个条件为真，那么用户 x 将不会向用户 y（x != y）发送好友请求：
+
+ages[y] <= 0.5 * ages[x] + 7
+ages[y] > ages[x]
+ages[y] > 100 && ages[x] < 100
+否则，x 将会向 y 发送一条好友请求。
+
+注意，如果 x 向 y 发送一条好友请求，y 不必也向 x 发送一条好友请求。另外，用户不会向自己发送好友请求。
+
+返回在该社交媒体网站上产生的好友请求总数。
+*/
+class Solution825 {
+public:
+	int numFriendRequests(vector<int>& ages) {
+		int* ageCnt = new int[200];
+		memset(ageCnt, 0, sizeof(int)*200);
+		int ans = 0;
+
+		for (int i = 0; i < ages.size(); i++)
+		{
+			ageCnt[ages[i]]++;
+		}
+
+		for (int i = 1; i < 121; i++)
+		{
+			for (int j = 1; j < 121; j++)
+			{
+				if (ageCnt[i] == 0 || j > i) {
+					continue;
+				}
+				if (!((j <= i/2 + 7) || j > i || (j > 100 && i < 100))) {
+					if (i == j)
+						ans += ageCnt[i] * (ageCnt[i] - 1);
+					else
+						ans += ageCnt[i] * (ageCnt[j]);
+				}
+			}
+		}
+		delete[] ageCnt;
+		return ans;
+	}
+};
+
+
+int main()
+{
+	Solution825 leetcode825;
+	vector<int> test{ 20,30,100,110,120 };
+	cout << leetcode825.numFriendRequests(test);
+  
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
