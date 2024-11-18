058f03eab2306c0640b7a26d9abd17a0ccbc7966
“xiez-njust-xmu” <xiez2000@163.com>
2024-11-18 09:14:53 +0800

    11.18-刷题625-3ms-击败89.47%

--- Immerse_in_LeetCode.cpp
+++ Immerse_in_LeetCode.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 /*
 825. 适龄的朋友
@@ -51,6 +52,45 @@ public:
 	}
 };
 
+/*
+661. 图片平滑器
+图像平滑器 是大小为 3 x 3 的过滤器，用于对图像的每个单元格平滑处理，平滑处理后单元格的值为该单元格的平均灰度。
+
+每个单元格的  平均灰度 定义为：该单元格自身及其周围的 8 个单元格的平均值，结果需向下取整。（即，需要计算蓝色平滑器中 9 个单元格的平均值）。
+
+如果一个单元格周围存在单元格缺失的情况，则计算平均灰度时不考虑缺失的单元格（即，需要计算红色平滑器中 4 个单元格的平均值）。
+*/
+class Solution625 {
+public:
+    vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
+		int m = img.size();
+		int n = img[0].size();
+        vector<vector<int>> sum(m+1, vector<int>(n+1, 0));
+		vector<vector<int>> ans(m, vector<int>(n, 0));
+		//前缀和
+		for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + img[i - 1][j - 1];
+            }
+        }
+		
+		 for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+				//四个边界
+                int a = max(0, i - 1);
+				int b = max(0, j - 1);
+                int c = min(m - 1, i + 1);
+				int d = min(n - 1, j + 1);
+				//格子数量
+                int cnt = (c - a + 1) * (d - b + 1);
+				
+                int tot = sum[c + 1][d + 1] - sum[a][d + 1] - sum[c + 1][b] + sum[a][b];
+                ans[i][j] = tot / cnt;
+            }
+        }
+		return ans;
+    }
+};
 
 int main()
 {
