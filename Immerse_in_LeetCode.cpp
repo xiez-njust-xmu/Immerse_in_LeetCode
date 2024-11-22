ec19a648c4e4c7530539686081828d5663deb36b
“xiez-njust-xmu” <xiez2000@163.com>
2024-11-22 11:01:59 +0800

    刷题117用时14ms击败30%

--- Immerse_in_LeetCode.cpp
+++ Immerse_in_LeetCode.cpp
@@ -387,7 +387,13 @@ public:
 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
 
 初始状态下，所有 next 指针都被设置为 NULL。*/
-class Solution116 {
+
+/*117. 填充每个节点的下一个右侧节点指针 II
+
+填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL 。
+
+初始状态下，所有 next 指针都被设置为 NULL 。*/
+class Solution116_117 {
 public:
     Node* connect(Node* root) {
         if(root == nullptr)
