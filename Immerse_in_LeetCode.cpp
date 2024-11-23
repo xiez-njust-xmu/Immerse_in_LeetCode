47033b1a11f86d99206ab458a94beb8078ac2486
xiez-njust-xmu <xiez2000@163.com>
2024-11-23 10:38:17 +0800

    每日一题3238用时0ms击败100%

--- Immerse_in_LeetCode.cpp
+++ Immerse_in_LeetCode.cpp
@@ -457,7 +457,7 @@ public:
 给定一个非负索引 rowIndex，返回「杨辉三角」的第 rowIndex 行。
 
 在「杨辉三角」中，每个数是它左上方和右上方的数的和。*/
-class Solution {
+class Solution119 {
 public:
 	vector<int> getRow(int rowIndex) {
 		vector<int> temp;
@@ -570,6 +570,44 @@ public:
 	}
 };
 
+/*3238. 求出胜利玩家的数目
+给你一个整数 n ，表示在一个游戏中的玩家数目。同时给你一个二维整数数组 pick ，其中 pick[i] = [xi, yi] 表示玩家 xi 获得了一个颜色为 yi 的球。
+
+如果玩家 i 获得的球中任何一种颜色球的数目 严格大于 i 个，那么我们说玩家 i 是胜利玩家。换句话说：
+
+如果玩家 0 获得了任何的球，那么玩家 0 是胜利玩家。
+如果玩家 1 获得了至少 2 个相同颜色的球，那么玩家 1 是胜利玩家。
+...
+如果玩家 i 获得了至少 i + 1 个相同颜色的球，那么玩家 i 是胜利玩家。
+请你返回游戏中 胜利玩家 的数目。
+
+注意，可能有多个玩家是胜利玩家。*/
+class Solution3238 {
+public:
+	int winningPlayerCount(int n, vector<vector<int>>& pick) {
+		int SUM[10][11];
+		memset(SUM, 0, sizeof(SUM));
+		for (auto& onepick : pick)
+		{
+			SUM[onepick[0]][onepick[1]]++;
+		}
+		int ans = 0;
+		for (int i = 0; i < n; i++)
+		{
+			//bool temp = false;
+			for (int j = 0; j <= 10; j++)
+			{
+				if (SUM[i][j] > i)
+				{
+					ans++;
+					break;
+				}
+					
+			}
+		}
+		return ans;
+	}
+};
 
 /*
 3243. 新增道路查询后的最短距离 I
