ce990dddf56434a3ba30d91f16abc4465fbf85e1
“xiez-njust-xmu” <xiez2000@163.com>
2024-11-25 11:14:25 +0800

    刷题122用时0ms击败100%

--- Immerse_in_LeetCode.cpp
+++ Immerse_in_LeetCode.cpp
@@ -485,6 +485,25 @@ public:
 		return Upper;
 	}
 };
+/*122. 买卖股票的最佳时机 II
+给你一个整数数组 prices ，其中 prices[i] 表示某支股票第 i 天的价格。
+
+在每一天，你可以决定是否购买和/或出售股票。你在任何时候 最多 只能持有 一股 股票。你也可以先购买，然后在 同一天 出售。
+
+返回 你能获得的 最大 利润 。
+*/
+class Solution122 {
+public:
+    int maxProfit(vector<int>& prices) {
+		int ans = 0;
+        for(int i =1;i<prices.size();i++)
+		{
+			if(prices[i]>prices[i-1])
+				ans+=(prices[i]-prices[i-1]);
+		}
+		return ans;
+    }
+};
 /*
 661. 图片平滑器
 图像平滑器 是大小为 3 x 3 的过滤器，用于对图像的每个单元格平滑处理，平滑处理后单元格的值为该单元格的平均灰度。
