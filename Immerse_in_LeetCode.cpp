8f7892ee94f4c2f83d3111dda542a579f496420e
xiez-njust-xmu <xiez2000@163.com>
2024-11-27 17:08:42 +0800

    每日一题3208用时0ms击败100%

--- Immerse_in_LeetCode.cpp
+++ Immerse_in_LeetCode.cpp
@@ -686,6 +686,35 @@ public:
 	}
 };
 
+/*3208. 交替组 II
+给你一个整数数组 colors 和一个整数 k ，colors表示一个由红色和蓝色瓷砖组成的环，第 i 块瓷砖的颜色为 colors[i] ：
+
+colors[i] == 0 表示第 i 块瓷砖的颜色是 红色 。
+colors[i] == 1 表示第 i 块瓷砖的颜色是 蓝色 。
+环中连续 k 块瓷砖的颜色如果是 交替 颜色（也就是说除了第一块和最后一块瓷砖以外，中间瓷砖的颜色与它 左边 和 右边 的颜色都不同），那么它被称为一个 交替 组。
+
+请你返回 交替 组的数目。
+
+注意 ，由于 colors 表示一个 环 ，第一块 瓷砖和 最后一块 瓷砖是相邻的。*/
+class Solution3208 {
+public:
+	int numberOfAlternatingGroups(vector<int>& colors, int k) {
+		int ans = 0;
+		int n = colors.size();
+		int cnt = 1;
+		for (int i = 0; i < n + k - 2; i++)
+		{
+			if (colors[i % n] != colors[(i + 1) % n])
+				cnt++;
+			else
+				cnt = 1;
+			if (cnt >= k)
+				ans++;
+		}
+		return ans;
+	}
+};
+
 /*3238. 求出胜利玩家的数目
 给你一个整数 n ，表示在一个游戏中的玩家数目。同时给你一个二维整数数组 pick ，其中 pick[i] = [xi, yi] 表示玩家 xi 获得了一个颜色为 yi 的球。
 
