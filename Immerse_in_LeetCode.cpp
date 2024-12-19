28fd430dad0610b645168e256eb46683befc2d42
Your Name <you@example.com>
2024-12-19 08:13:58 +0800

    每日一题3285用时0ms击败100%

--- Immerse_in_LeetCode.cpp
+++ Immerse_in_LeetCode.cpp
@@ -1285,6 +1285,26 @@ public:
 		return (c1 % 2 == c2 % 2);
 	}
 };
+
+/*3285. 找到稳定山的下标
+
+有 n 座山排成一列，每座山都有一个高度。给你一个整数数组 height ，其中 height[i] 表示第 i 座山的高度，再给你一个整数 threshold 。
+
+对于下标不为 0 的一座山，如果它左侧相邻的山的高度 严格大于 threshold ，那么我们称它是 稳定 的。我们定义下标为 0 的山 不是 稳定的。
+
+请你返回一个数组，包含所有 稳定 山的下标，你可以以 任意 顺序返回下标数组。*/
+class Solution3285 {
+public:
+    vector<int> stableMountains(vector<int>& height, int threshold) {
+        vector<int> ans;
+		for(int i=0;i<height.size()-1;i++)
+		{
+			if(height[i]>threshold)
+				ans.push_back(i+1);
+		}
+		return ans;
+    }
+};
 int main()
 {
 	Solution151 s;
